b8d1044114618d4fccfbb412446c41c0e41d3d63
Ricardo José Nunes <ricardo.nunes@compasso.com.br>
2021-12-15 15:08:18 -0300

    The dependency app

---
+++ appdependecy/src/main/java/com/example/appdependecy/AppdependecyApplication.java
@@ -0,0 +1,13 @@
+package com.example.appdependecy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppdependecyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppdependecyApplication.class, args);
+	}
+
+}
---
+++ appdependecy/src/test/java/com/example/appdependecy/AppdependecyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appdependecy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppdependecyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
