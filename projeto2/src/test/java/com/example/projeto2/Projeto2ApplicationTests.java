8450f5157010fe2e62c834c79581df0646116df0
Ricardo J. Nunes <jnunes.ricardo@gmail.com>
2024-11-07 18:16:52 -0300

    Implement a simple test

--- projeto2/src/test/java/com/example/projeto2/Projeto2ApplicationTests.java
+++ projeto2/src/test/java/com/example/projeto2/Projeto2ApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.projeto2;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Projeto2ApplicationTests {
 
+	@Autowired
+	private BinarySearchController binarySearchController;
+
 	@Test
 	void contextLoads() {
+		assertThat(binarySearchController).isNotNull();
 	}
 
 }
